b92c75733d92627ff541ab63f873d52c3923304c
omid <omoghadasi@gmail.com>
2024-08-10 22:26:58 +0330

    create route param

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUser } from './dtos/CreateUser.dto';
 
 @Controller('users')
@@ -11,5 +11,10 @@ export class UsersController {
   @Post('create')
   createUser(@Body() userData: CreateUser) {
     return userData;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return { id };
   }
 }
