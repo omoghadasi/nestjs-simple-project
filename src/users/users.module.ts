7f7aab5a09380f542bf0116ca912872ba9f4ffb2
omid <omoghadasi@gmail.com>
2024-08-11 20:25:52 +0330

    add example middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ExampleMiddleware } from './example.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ExampleMiddleware).forRoutes(UsersController);
+  }
+}
